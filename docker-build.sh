9505c6fbe2a7998793196b29edf2f0d9a40509f3
wallace_tan <wallace_tan@hive.gov.sg>
2021-09-20 12:04:50 +0800

    Update to alpine 3.12 > 3.14

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-VERSION=2019.8.20
+VERSION=2021.8.29
 docker build -f dnscrypt/Dockerfile -t dnscrypt:${VERSION} .
 docker build -f dnsmasq/Dockerfile -t dnsmasq:${VERSION} .
-docker-compose -f docker-compose.yml up
+docker compose -f docker-compose.yml up -d
+#docker-compose -f docker-compose.yml up
 # Visit http://<docker-host>:5380, authenticate with foo/bar and you should see
 # http://foo:bar@localhost:5380
-# docker-compose -f docker-compose.yml up
+# docker compose -f ~/Projects/Docker/localdns/docker-compose.yml ps
+# docker compose -f docker-compose.yml up
+# docker compose -f docker-compose.yml up -d
+
+# sudo networksetup -listallnetworkservices
+# sudo networksetup -getdnsservers Wi-Fi
+# sudo networksetup -setdnsservers Wi-Fi empty
+# sudo networksetup -setdnsservers Wi-Fi 127.0.0.1
+# sudo networksetup -setdnsservers Wi-Fi 1.1.1.1
