b9e41b15880b47902c8c12ffd1045d883686a933
wallace_tan <wallace_tan@hive.gov.sg>
2022-02-03 16:55:41 +0800

    Updated docker build version 2022.02.03 and getadservers.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-VERSION=2021.8.29
-docker build -f dnscrypt/Dockerfile -t dnscrypt:${VERSION} .
-docker build -f dnsmasq/Dockerfile -t dnsmasq:${VERSION} .
+VERSION=2022.02.03
+docker build --no-cache -f dnscrypt/Dockerfile -t dnscrypt:${VERSION} .
+docker build --no-cache -f dnsmasq/Dockerfile -t dnsmasq:${VERSION} .
 docker compose -f docker-compose.yml up -d
 #docker-compose -f docker-compose.yml up
 # Visit http://<docker-host>:5380, authenticate with foo/bar and you should see
 # http://foo:bar@localhost:5380
+
 # docker compose -f ~/Projects/Docker/localdns/docker-compose.yml ps
+# docker compose ps
+# docker compose up -d
+# docker compose logs dnsmasq
 # docker compose -f docker-compose.yml up
 # docker compose -f docker-compose.yml up -d
+# docker compose -f docker-compose.yml down
+# docker compose -f ~/Projects/Docker/localdns/docker-compose.yml logs dnsmasq -f
 
 # sudo networksetup -listallnetworkservices
 # sudo networksetup -getdnsservers Wi-Fi
 # sudo networksetup -setdnsservers Wi-Fi empty
 # sudo networksetup -setdnsservers Wi-Fi 127.0.0.1
 # sudo networksetup -setdnsservers Wi-Fi 1.1.1.1
+
+#https://celeritascdn.com/prod/redirect.html?lu=https%3A%2F%2Fc.lazada.sg%2Ft%2Fc.bFaFjE%3Fsub_id1%3D1221179-Desktop-ADC
+# dig celeritascdn.com
+# dig 474F21Nov75
--- etc/getadservers.sh
+++ etc/getadservers.sh
@@ -26,31 +26,45 @@ reloadcmd='/usr/sbin/dnsmasq --no-daemon restart'
 
 # temp files to use
 tmpfile="/etc/dnsmasq.d/adservers.tmp"
-tmpconffile="/etc/dnsmasq.d/adservers.conf"
+newconffile="/etc/dnsmasq.d/adservers.conf.new"
+conffile="/etc/dnsmasq.d/adservers.conf"
 
+# delete $tmpfile
+echo "********************************************************************************"
+echo ">>> Delete $tmpfile"
+rm $tmpfile
 # command to fetch the list (alternatives commented out)
 fetchcmd="/usr/bin/wget -q -O $tmpfile $listurl"
-
+echo "********************************************************************************"
+echo ">>> $fetchcmd"
 $fetchcmd
 
-# add the extras
- [ -f "$extrasfile" ]  && cat $extrasfile >> $tmpfile
+# add $extrasfile to $tmpfile
+echo "********************************************************************************"
+echo ">>> Add $extrasfile to $tmpfile"
+[ -f "$extrasfile" ]  && cat $extrasfile >> $tmpfile
 
 # check the temp file exists OK before overwriting the existing list
 if  [ ! -s $tmpfile ]
 then
-echo "temp file '$tmpfile' either doesn't exist or is empty; quitting"
+echo "********************************************************************************"
+echo ">>> temp file '$tmpfile' either doesn't exist or is empty; quitting"
 exit
 fi
 
 # get a fresh list of ad server addresses for dnsmasq to refuse
-#cat $configfile | grep -v "address=" > $tmpconffile
+#cat $configfile | grep -v "address=" > $newconffile
 
+echo "********************************************************************************"
 while read line; do
     ADDRESS="/${line}/${addcatcherip}"
-    echo "address=\"${ADDRESS}\"" >> $tmpconffile
-done < $tmpfile
+    #echo "address=\"${ADDRESS}\"" >> $newconffile
+    echo "address=\"${ADDRESS}\"" | tee -a $newconffile
+done < <(sort $tmpfile | uniq -u)
 
 # $reloadcmd
-# rm $tmpfile
-exit
+echo "********************************************************************************"
+echo ">>> Delete $tmpfile"
+##rm $tmpfile
+##cp $newconffile $conffile
+exit 0
